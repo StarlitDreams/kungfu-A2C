3cf955bcfe697489de472600facd05bd96c0a17d
Lena Ayen <hari020204@gmail.com>
2025-07-18 21:53:16 +0530

    Update mian.py

--- mian.py
+++ mian.py
@@ -13,4 +13,25 @@ import gymnasium as gym
 from gymnasium import ObservationWrapper
 from gymnasium.spaces import Box
 
-class Network
+class Network(nn.Module):
+    def __init__(self, action_size):
+        super(Network, self).__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels=4, out_channels=32, kernel_size=(3,3), stride=2)
+        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3), stride=2)
+        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=2)
+        self.flatten = torch.nn.Flatten()
+        self.fc1a = torch.nn.Linear(512,128)
+        self.fc2a = torch.nn.Linear(128, action_size)
+        self.fc2s = torch.nn.Linear(128,1)
+        
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = self.flatten(x)
+        x = F.relu(self.fc1a(x))
+        action_probs = F.softmax(self.fc2a(x), dim=-1)
+        state_value = self.fc2s(x)
+        return action_probs, state_value
+                 
